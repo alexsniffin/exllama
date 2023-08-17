1deac95a1eb7d55d785dcb01f763541b65b3b0aa
alexsniffin <alexsniffin@gmail.com>
2023-08-17 00:17:50 -0400

    update

--- example_flask.py
+++ example_flask.py
@@ -1,92 +1,113 @@
-from model import ExLlama, ExLlamaCache, ExLlamaConfig
-from flask import Flask, request
-from tokenizer import ExLlamaTokenizer
-from generator import ExLlamaGenerator
-import os, glob
+import glob
+import os
+import time
 
+from flask import Flask
+from flask import request, jsonify
+from threading import Lock
+from datetime import datetime
+
+from generator import ExLlamaGenerator
+from model import ExLlama, ExLlamaCache, ExLlamaConfig
+from tokenizer import ExLlamaTokenizer
+
+print(f"Starting API")
 # Directory containing config.json, tokenizer.model and safetensors file for the model
-model_directory = "/mnt/str/models/llama-7b-4bit/"
+model_directory = "/data/model"
 
 tokenizer_path = os.path.join(model_directory, "tokenizer.model")
 model_config_path = os.path.join(model_directory, "config.json")
 st_pattern = os.path.join(model_directory, "*.safetensors")
 model_path = glob.glob(st_pattern)[0]
 
-config = ExLlamaConfig(model_config_path)               # create config from config.json
-config.model_path = model_path                          # supply path to model weights file
+config = ExLlamaConfig(model_config_path)  # create config from config.json
+config.model_path = model_path  # supply path to model weights file
 
-model = ExLlama(config)                                 # create ExLlama instance and load the weights
+model = ExLlama(config)  # create ExLlama instance and load the weights
 print(f"Model loaded: {model_path}")
 
-tokenizer = ExLlamaTokenizer(tokenizer_path)            # create tokenizer from tokenizer model file
-cache = ExLlamaCache(model)                             # create cache for inference
-generator = ExLlamaGenerator(model, tokenizer, cache)   # create generator
-
-# Flask app
+tokenizer = ExLlamaTokenizer(tokenizer_path)  # create tokenizer from tokenizer model file
+cache = ExLlamaCache(model)  # create cache for inference
+generator = ExLlamaGenerator(model, tokenizer, cache)  # create generator
 
 app = Flask(__name__)
+request_lock = Lock()
 
 
-# Inference with settings equivalent to the "precise" preset from the /r/LocalLLaMA wiki
+@app.route('/v1/chat/completions', methods=['POST'])
+def completions():
+    with request_lock:
+        data = request.json
 
-@app.route('/infer_precise', methods=['POST'])
-def inferContextP():
-    print(request.form)
-    prompt = request.form.get('prompt')
+        messages = data['messages']
 
-    generator.settings.token_repetition_penalty_max = 1.176
-    generator.settings.token_repetition_penalty_sustain = config.max_seq_len
-    generator.settings.temperature = 0.7
-    generator.settings.top_p = 0.1
-    generator.settings.top_k = 40
-    generator.settings.typical = 0.0    # Disabled
+        temperature = data.get('temperature', 1.0)
+        top_p = data.get('top_p', 1.0)
+        max_tokens = data.get('max_tokens', 1024)
+        presence_penalty = data.get('presence_penalty', 1.2)
 
-    outputs = generator.generate_simple(prompt, max_new_tokens = 200)
-    return outputs
+        # System prompt
+        system_prompt = """
+        <s>[INST] <<SYS>>
+        You will provide dialogue to the conversation.
+        <</SYS>>
+        """
 
+        # Start with the system prompt for the first message
+        if len(messages) == 1:
+            prompt = system_prompt
+            user_msg = messages[0]['role']
+            model_answer = messages[0]['content']
+            prompt += f"{user_msg} [/INST] {model_answer}</s>"
+        else:
+            # For subsequent messages, append the interaction
+            prompt = ""
+            for message in messages:
+                user_msg = message['role']
+                model_answer = message['content']
+                prompt += f"<s>[INST] {user_msg} [/INST] {model_answer}</s>"
 
-# Inference with settings equivalent to the "creative" preset from the /r/LocalLLaMA wiki
+        print(prompt)
 
-@app.route('/infer_creative', methods=['POST'])
-def inferContextC():
-    print(request.form)
-    prompt = request.form.get('prompt')
+        generator.settings.token_repetition_penalty_max = presence_penalty
+        generator.settings.token_repetition_penalty_sustain = config.max_seq_len
+        generator.settings.temperature = temperature
+        generator.settings.top_p = top_p
+        generator.settings.top_k = 40
+        generator.settings.typical = 0.25
 
-    generator.settings.token_repetition_penalty_max = 1.1
-    generator.settings.token_repetition_penalty_sustain = config.max_seq_len
-    generator.settings.temperature = 0.72
-    generator.settings.top_p = 0.73
-    generator.settings.top_k = 0        # Disabled
-    generator.settings.typical = 0.0    # Disabled
+        output_content = generator.generate_simple(prompt, max_new_tokens=max_tokens)
 
-    outputs = generator.generate_simple(prompt, max_new_tokens = 200)
-    return outputs
+        messages.append({
+            "role": "assistant",
+            "content": output_content
+        })
 
+        response = {
+            "id": "generated",
+            "object": "chat.completion",
+            "created": int(time.mktime(datetime.now().timetuple())),
+            "model": "gpt-3.5-turbo-0613",
+            "choices": [{
+                "index": 0,
+                "message": messages[-1],
+                "finish_reason": "stop"
+            }],
+            "usage": {
+                "prompt_tokens": 0,
+                "completion_tokens": 0,
+                "total_tokens": 0
+            }
+        }
 
-# Inference with settings equivalent to the "sphinx" preset from the /r/LocalLLaMA wiki
+        return jsonify(response)
 
-@app.route('/infer_sphinx', methods=['POST'])
-def inferContextS():
-    print(request.form)
-    prompt = request.form.get('prompt')
-
-    generator.settings.token_repetition_penalty_max = 1.15
-    generator.settings.token_repetition_penalty_sustain = config.max_seq_len
-    generator.settings.temperature = 1.99
-    generator.settings.top_p = 0.18
-    generator.settings.top_k = 30
-    generator.settings.typical = 0.0    # Disabled
-
-    outputs = generator.generate_simple(prompt, max_new_tokens = 200)
-    return outputs
-
-
-# Start Flask app
 
 host = "0.0.0.0"
-port = 8004
-print(f"Starting server on address {host}:{port}")
+port = 8080
+print(f"Server started on address {host}:{port}")
 
 if __name__ == '__main__':
     from waitress import serve
-    serve(app, host = host, port = port)
+
+    serve(app, host=host, port=port)
